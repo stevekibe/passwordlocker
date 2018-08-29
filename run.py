6ca11846c9de487be3beb26e8744119b30697afa
stevekibe <stevekibe2345.gmail.com>
2018-08-29 20:00:21 +0300

    add the saving   code in run.py

--- run.py
+++ run.py
@@ -88,60 +88,74 @@ def generate_password():
     #layout 
 
 def main():
-    print("Welcome to password locker....Feel safe to use our services")
-    print("\n")
-    while True:
-        print('*'*150)
-        short_code = input("Use the following short codes: CU- create a new user account,  LN - to log in if you have an account ,  EX - exit from password locker \n ").lower().strip()
-        print ('+'*60)
+        print("Welcome to password locker....Feel safe to use our services")
+        print("\n")
+        while True:
+                print('*'*150)
+                short_code = input("Use the following short codes: CU- create a new user account,  LN - to log in if you have an account ,  EX - exit from password locker \n ").lower().strip()
+                print ('+'*60)
         
-        if short_code == "EX":
-                print("Thank you for using password locker will see you again ")
-                break
+                if short_code == "EX":
+                        print("Thank you for using password locker will see you again ")
+                        break
         
-        elif short_code == "CA":
-                print("Sign up")
-                print('-'*30)
-                user_name = input("Username: ")
-                password = input("Password: ")
-                email = input("Email: ")
+                elif short_code == "CA":
+                        print("Sign up")
+                        print('-'*30)
+                        user_name = input("Username: ")
+                        password = input("Password: ")
+                        email = input("Email: ")
 
-                save_user(create_user(user_name,password,email))
-                print('\n')
-                print(f"{user_name} Your account has been created successfully")
+                        save_user(create_user(user_name,password,email))
+                        print('\n')
+                        print(f"{user_name} Your account has been created successfully")
 
-        elif code == "LN":
-            print("Enter your User name and your password to log in:")  
-            print('*'*30)
-            user_name = input("Username : ")
-            password = input("Password: ")
-            sign_in = login_user(user_name,password)
-            if sign_in == True:
-                    print(f"{user_name} You are now logged in")
-                while True:
-                    print("Use the follwing to navigate through: CA-to create new account DL- to see the list of your accounts EX - toexit current account")
-                    if code == "CA":
-                        print("Create new account")
-                        print('='*40)
-                        account_name = input("Account name \ Site name: ")
-                        account_username = input("Site user name: ")
-                        print('.'*60)
-                        password_option = input("You can choose between: EP- to input existing password GP- to generate new password")
-                        print('*'*80)
-                        while True:
-                                if password_option == "EP":
-                                        account_password = input("Enter your password: ")
-                                        break
-                                elif password_option == "GP":
-                                        account_password = generate_password()
-                                        break
-                                else:
-                                        print("Invalid Entry!")
-                                        break
-                                save_profileinfo(create_profileinfo(account_name,account_username, account_password))
-                                print('+'*40)
-                                print(f"New account created: \n Account:{account_name}\n User Name:{account_username}\n Password:{account_password}")
-                                print('*' * 40)
+                elif code == "LN":
+                        print("Enter your User name and your password to log in:")  
+                        print('*'*30)
+                        user_name = input("Username : ")
+                        password = input("Password: ")
+                        sign_in = login_user(user_name,password)
+                        if sign_in == True:
+                                print(f"{user_name} You are now logged in")
+                                while True:
+                                        print("Use the follwing to navigate through: CA-to create new account DL- to see the list of your accounts EX - toexit current account")
+                                        if code == "CA":
+                                                print("Create new account")
+                                                print('='*40)
+                                                account_name = input("Account name \ Site name: ")
+                                                account_username = input("Site user name: ")
+                                                print('.'*60)
+                                                password_option = input("You can choose between: EP- to input existing password GP- to generate new password")
+                                                print('*'*80)
+                                                while True:
+                                                        if password_option == "EP":
+                                                                account_password = input("Enter your password: ")
+                                                                break
+                                                        elif password_option == "GP":
+                                                                account_password = generate_password()
+                                                                break
+                                                        else:
+                                                                print("Invalid Entry!")
+                                                                break
+                                                        save_profileinfo(create_profileinfo(account_name,account_username, account_password))
+                                                        print('+'*40)
+                                                        print(f"New account created: \n Account:{account_name}\n User Name:{account_username}\n Password:{account_password}")
+                                                        print('*' * 40)
+
+                                                if short_code == "da":
+                                                        if display_accounts():
+                                                        print("Here's your list of accounts(s): ")
+
+                                                        print('#'* 30)
+                                                        for account in display_accounts():
+                                                                print(f"Site:{account.account_name} \n User Name:{account_username} \n Password:{account_password}")
+                                                                print('*'*30)
+                                                        else:
+                                                        print("Sorry ... you do not have an account yet")
+
+                                        
+
 
                         
 
